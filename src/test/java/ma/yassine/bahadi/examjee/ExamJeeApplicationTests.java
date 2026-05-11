d517ab4376b0f68102cced8492347f335859ff1b
YassineBahadi <yassinebahadi04@gmail.com>
2026-05-11 08:54:24 +0100

    Initial commit

---
+++ src/test/java/ma/yassine/bahadi/examjee/ExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.yassine.bahadi.examjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
